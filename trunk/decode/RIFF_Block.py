b6fffe9259798dec857dff3043c537a06802c5e1
skylined@chromium.org <skylined@chromium.org@187e915e-1ff6-11df-b3bf-63d83f449e3e>
2010-06-16 07:55:58 +0000

    RIFF Block/Chunk + Added a lot of extra chunks/blocks, information and checks

--- trunk/decode/RIFF_Block.py
+++ trunk/decode/RIFF_Block.py
@@ -16,16 +16,21 @@ from Structure import Structure
 
 class RIFF_Block(Structure):
   type_name = 'RIFF_BLOCK';
-  def __init__(self, stream, offset, max_size, parent, name, valid_block_names):
+  def __init__(self, stream, offset, max_size, parent, name, valid_blocks):
     import C;
     from RIFF_Chunk import RIFF_Chunk;
     from RIFF_ACON_Checks import RIFF_ACON_Checks;
+    from RIFF_WAVE_Checks import RIFF_WAVE_Checks;
     Structure.__init__(self, stream, offset, max_size, parent, name);
 
     self._name = self.Member(C.STRING, 'name', 4);
 
-    if valid_block_names is not None:
-      if self._name.value not in valid_block_names:
+    valid_chunk_names = None;
+    if valid_blocks is not None:
+      valid_block_names = valid_blocks.keys();
+      if self._name.value in valid_block_names:
+        valid_chunk_names = valid_blocks[self._name.value];
+      else:
         if len(valid_block_names) == 1:
           valid_block_names_string = valid_block_names[0];
         else:
@@ -37,16 +42,6 @@ class RIFF_Block(Structure):
     else:
       self._name.notes.append('unknown/unexpected');
 
-    if self._name.value == 'ACON':
-      valid_chunk_names = {'anih': None, 'rate': None, \
-          'LIST': ['fram', 'INFO'], 'INAM': None, 'IART': None};
-    elif self._name.value == 'fram':
-      valid_chunk_names = {'icon': None};
-    elif self._name.value == 'INFO':
-      valid_chunk_names = {'INAM': None, 'IART': None};
-    else:
-      valid_chunk_names = None;
-
     self._padding = [];
     self._chunks = [];
     self._named_chunks = {};
@@ -54,6 +49,16 @@ class RIFF_Block(Structure):
       chunk = self.Member(RIFF_Chunk, 'chunk_%d' % (len(self._chunks) + 1), \
           valid_chunk_names);
       self._chunks.append(chunk);
+      off_by = self.current_offset - chunk.expected_end_offset;
+      if self.DataAvailable() and off_by > 0:
+        # The RIFF_Block that is contained in this RIFF_Chunk was larger than
+        # expected and our offset and max_size reflect this.This means the
+        # current_offset now points inside the next RIFF_Chunk and not to the
+        # start of it. We need to move the offset back to where the next
+        # RIFF_Chunk is expected to start and update the max_size as well:
+        self.current_offset -= off_by;
+        self.current_max_size += off_by;
+
       if chunk._name.value not in self._named_chunks:
         self._named_chunks[chunk._name.value] = [chunk];
       else:
@@ -61,8 +66,27 @@ class RIFF_Block(Structure):
 
     if self._name.value == 'ACON':
       RIFF_ACON_Checks(self);
+    elif self._name.value == 'WAVE':
+      RIFF_WAVE_Checks(self);
     self.notes.append( \
         '0x%X|%d chunks' % (len(self._chunks), len(self._chunks)));
+
+    chunks_format_details = {};
+    for chunk in self._chunks:
+      if chunk.format_details not in chunks_format_details:
+        chunks_format_details[chunk.format_details] = 1;
+      else:
+        chunks_format_details[chunk.format_details] += 1;
+    keys = chunks_format_details.keys();
+    keys.sort();
+    details = [];
+    for key in keys:
+      if chunks_format_details[key] == 1:
+        details.append(key);
+      else:
+        details.append('%d * %s' % (chunks_format_details[key], key));
+    self.format_details = '%s(%s)' % \
+        (self._name.value, ', '.join(details));
 
     self.Unused();
 
