23a1945520b06a28d2ced5e2446f9ae1e184f17c
skyLined <skylined@chromium.org>
2010-06-16 06:28:54 +0000

    Add more structure to ANI file

--- decode/ANI.py
+++ decode/ANI.py
@@ -16,5 +16,22 @@ import C;
 from RIFF_Chunk import RIFF_Chunk;
 
 def ANI(stream, offset, max_size, parent, name):
-  result = RIFF_Chunk(stream, offset, max_size, parent, name, {'RIFF': ['ACON']});
+  result = RIFF_Chunk(stream, offset, max_size, parent, name, {
+    'RIFF': {
+      'ACON': {
+        'anih': None,
+        'rate': None,
+        'seq ': None,
+        'LIST': {
+          'fram': {
+            'icon': None
+          }, 
+          'INFO': {
+            'INAM': None,
+            'IART': None
+          },
+        },
+      },
+    }
+  });
   return result;
