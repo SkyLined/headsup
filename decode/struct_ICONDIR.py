0333eeb721d732f20f7edd23d336e1020b389570
skyLined <skylined@chromium.org>
2010-06-16 07:52:26 +0000

    struct_ICONDIR + format_details

--- decode/struct_ICONDIR.py
+++ decode/struct_ICONDIR.py
@@ -22,13 +22,17 @@ def struct_ICONDIR(stream, offset, max_size, parent, name):
       ('Type',            C.WORD),   # idType (1=.ICO, 2=.CUR)
       ('Count',           C.WORD),   # idCount (number of images)
   );
+  format_details = [];
   if result._Reserved.value != 0:
     result._Reserved.warnings.append('expected value to be 0');
   if result._Type.value not in [1, 2]:
     result._Type.warnings.append('expected value to be 1 or 2');
+    format_details.append('unknown');
   else:
-    result._Type.notes.append( \
-        {1:'ICON', 2:'CURSOR'}[result._Type.value]);
+    result._Type.notes.append({1:'ICON', 2:'CURSOR'}[result._Type.value]);
+    format_details.append({1:'icon', 2:'cursor'}[result._Type.value]);
   if result._Count.value == 0:
     result._Count.warnings.append('expected at least one image');
+
+  result.format_details = ', '.join(format_details);
   return result;
